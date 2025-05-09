dad5a7dff9101454cf0e62d42ddf8ba6d78be918
Pierre Roux <pierre.roux@onera.fr>
2025-05-09 10:55:56 +0200

    Test coqPackages.metacoq 1.4

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/3bb8fa02e657bf0d87727ccab12e30c2e11a057b.tar.gz";
-    sha256 = "0fp1fcwmpzimg9m6ak2lzrfr29z8h8rvlxanm49682a597x3vrz1";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/d79a2b8005c5e2bd2df0aaca600789f2dafcc616.tar.gz";
+         sha256 = "0xxi4pvi1251wfbprw8bs7vps9fggjskzwhx7zs4xszbsxhl9ydk";
+       }
