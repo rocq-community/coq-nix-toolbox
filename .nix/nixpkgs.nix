10eb9fb8d3f36480ddc693099aa03866b31d2ab9
Pierre Roux <pierre.roux@onera.fr>
2025-05-09 18:41:44 +0200

    coqPackages.mathcomp: 2.3.0 -> 2.4.0
    Following merge of https://github.com/NixOS/nixpkgs/pull/398851

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f7f8fc4685c7c56d2238746e00fe826005b8e0c6.tar.gz";
-    sha256 = "128262dff54lcns5lbgqrl9vxsdw7n9rk5517mgfvinxhvv68p7y";
+    url = "https://github.com/NixOS/nixpkgs/archive/3bb8fa02e657bf0d87727ccab12e30c2e11a057b.tar.gz";
+    sha256 = "0fp1fcwmpzimg9m6ak2lzrfr29z8h8rvlxanm49682a597x3vrz1";
   }
