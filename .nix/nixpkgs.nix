23d450c4b9c43c8d13b0dea44e2e3444df7993ca
Lucie <github@tweqx.fr>
2025-05-14 17:31:31 +0200

    Test Nixpkgs rocq-elpi update PR

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/d026ceca14fe50451c9bc43af676d922f8826b7d.tar.gz";
-    sha256 = "02g1ymc11z86hv99plp93k88byybjaqsc2hazb1yis53lwzb9hr6";
-  }
+         url = "https://github.com/lweqx/nixpkgs/archive/dc9965bd859caa7b75543b136fbfc95092d951ab.tar.gz";
+         sha256 = "1gb7jb4fv5g42klk5alc17m1fw92ajyhq9jg76fd649nkcddf59j";
+       }
