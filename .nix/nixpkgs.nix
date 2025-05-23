746aed2791961c830f6c5c9cdc7c28a755650e2f
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-05-23 12:25:16 +0200

    coqPackages.RustExtraction 0.1.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/be58fd03a9f66cddb455b04347c245e61527934a.tar.gz";
-    sha256 = "15gpj093gc80ws83rv4lvvs9c6rrvi8i4nq0ixvdccxc54m9m7sl";
-  }
+         url = "https://github.com/4ever2/nixpkgs/archive/831b15dab3c5e1b2884139264582e1ec5e02c688.tar.gz";
+         sha256 = "0c9n00ws995shkbidv34igh09n6vk4gl9b2dnsn1fgqfdpigcwkv";
+       }
