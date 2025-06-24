efbcc64d30a17cfb70b8bf25f878e5068110fe9b
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2025-06-24 17:56:20 +0200

    Add a flake layer on top of the current infrastructure.
    Use explicit system for purity.
    
    Replace dots in bundle names to make the CLI usable.
    
    Co-authored-by: VojtechStep <vojtechstepancik@outlook.com>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "The Rocq Nix Toolbox";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    rec {
+      lib = rec {
+        forAllSystems =
+          function:
+          nixpkgs.lib.genAttrs [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ] function;
+        allPackages =
+          src:
+          with nixpkgs.lib;
+          forAllSystems (
+            system:
+            let
+              underlyingNix = (import ./default.nix { inherit system src; });
+              instances = underlyingNix.passthru.setup.instances;
+              defaultInstance =
+                underlyingNix.passthru.setup.instances.${underlyingNix.passthru.setup.config.default-bundle};
+              jobsList = mapAttrsToList (
+                bundleName: instance:
+                let
+                  listOfJobs = map (jobName: {
+                    name = "${builtins.replaceStrings [ "." ] [ "-" ] bundleName}:${jobName}";
+                    value = instance.pkgs.rocqPackages.${jobName} or instance.pkgs.coqPackages.${jobName};
+                  }) instance.jobs;
+                in
+                listToAttrs listOfJobs
+              ) instances;
+            in
+            (foldl mergeAttrs { } jobsList)
+            // {
+              default = defaultInstance.this-pkg;
+            }
+          );
+      };
+      packages = lib.allPackages ./.;
+    };
+}
