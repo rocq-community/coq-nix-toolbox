0398def696bd0b69d470d787517f57558a2f4327
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2025-06-26 17:43:28 +0200

    Make toolbox installable with a flake template.

--- flake.nix
+++ flake.nix
@@ -4,47 +4,48 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {nixpkgs, ...}:
-  let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] function;
-  in {
-    lib = {
-      
+  outputs =
+    { nixpkgs, ... }:
+    rec {
+      lib = rec {
+        forAllSystems =
+          function:
+          nixpkgs.lib.genAttrs [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ] function;
+        allPackages =
+          src:
+          with nixpkgs.lib;
+          forAllSystems (
+            system:
+            let
+              underlying-nix = (import ./default.nix { inherit system src; });
+              instances = underlying-nix.passthru.setup.instances;
+              selected-instance = underlying-nix.passthru.setup.selected-instance;
+              jobsList = mapAttrsToList (
+                bundleName: instance:
+                let
+                  listOfJobs = map (jobName: {
+                    name = "${builtins.replaceStrings [ "." ] [ "-" ] bundleName}_${jobName}";
+                    value = instance.pkgs.rocqPackages.${jobName} or instance.pkgs.coqPackages.${jobName};
+                  }) instance.jobs;
+                in
+                listToAttrs listOfJobs
+              ) instances;
+            in
+            (foldl mergeAttrs { } jobsList)
+            # // {
+            #   default = selected-instance.this-pkg;
+            # }
+          );
+      };
+      templates.default = {
+        path = ./template;
+        description = "Initialize the Rocq Nix Toolbox in a new project";
+      };
+      packages = lib.allPackages ./.;
     };
-    templates = {
-      #default = ...;
-    };
-    packages = with nixpkgs.lib; forAllSystems (system:
-      let
-        underlying-nix = (import ./default.nix { inherit system; });
-        instances = underlying-nix.passthru.setup.instances;
-        selected-instance = underlying-nix.passthru.setup.selected-instance;
-        jobsList =
-          mapAttrsToList
-            (bundleName: instance:
-              let
-                listOfJobs =
-                  map
-                    (jobName: {
-                      name = "${builtins.replaceStrings ["."] ["-"] bundleName}_${jobName}";
-                      value = instance.pkgs.rocqPackages.${jobName} or
-                              instance.pkgs.coqPackages.${jobName};
-                    })
-                    instance.jobs;
-              in
-              listToAttrs listOfJobs)
-            instances;
-      in
-      (foldl mergeAttrs {} jobsList)
-      # // {
-      #   default = selected-instance.this-pkg;
-      # }
-    );
-  };
 }
