079091108a57e2ded4246297f5bd0a0eb73ec1ff
Pierre Roux <pierre.roux@onera.fr>
2025-06-29 17:23:01 +0200

    coqPackages.*: better formatting fix
    Following the merge of https://github.com/NixOS/nixpkgs/pull/421022
    and https://github.com/NixOS/nixpkgs/pull/421216

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2d23937f55fa3b09b0d2d762980d9c7bc56a7787.tar.gz";
-    sha256 = "0s9gdxqph0wyia067ypmpdwnmsix561x78nf7kavc9n3n0m8k54n";
+    url = "https://github.com/NixOS/nixpkgs/archive/7321d5dc7b0b2722d03c67bc8851acd014ec446c.tar.gz";
+    sha256 = "0nfxpbky01m1bad4xi336450cnlfss456fiw0ziq3i725ybga5mc";
   }
