56d4c449a0e9ac04ba179d28cf461fbd97ed456d
Pierre Roux <pierre.roux@onera.fr>
2025-07-03 17:58:43 +0200

    coqPackages.VST: adapt to master
    Following merge of https://github.com/NixOS/nixpkgs/pull/422128

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/54a7c3bc87f7768442175b42757bf8f3229cd03c.tar.gz";
-    sha256 = "0xpd7rnmcfai121kaw20d1m9wrvi4v9vpsg7ydx7520hp9yfr6yz";
+    url = "https://github.com/NixOS/nixpkgs/archive/79e56adbafe15dd49572571d93fe97327e5ba0df.tar.gz";
+    sha256 = "11l29m0dyvg1yvfj3blmaw4gnqyxi455jcx2rbh4m4rr1p8nzkhx";
   }
