286c4fafac33ced328f506eea2ef0f6d838bf517
Pierre Roux <pierre.roux@onera.fr>
2025-07-07 09:29:41 +0200

    coqPackages.mathcomp-analysis: 1.11.0 -> 1.12.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/79e56adbafe15dd49572571d93fe97327e5ba0df.tar.gz";
-    sha256 = "11l29m0dyvg1yvfj3blmaw4gnqyxi455jcx2rbh4m4rr1p8nzkhx";
+    url = "https://github.com/NixOS/nixpkgs/archive/d2cb712ff42e36a466e25131b17a5403c7b3d4f3.tar.gz";
+    sha256 = "13gk25yybjyki1sqs6dlw1971vhabsbzv156hcghb4pdgcip2hbw";
   }
