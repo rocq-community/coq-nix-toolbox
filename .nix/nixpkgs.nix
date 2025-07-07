4c0ee0af111f4032e093b3cd09a885d32d3fb07d
Pierre Roux <pierre.roux@onera.fr>
2025-07-07 10:50:57 +0200

    coqPackages.fcsl-pcm: inti at 2.2.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/79e56adbafe15dd49572571d93fe97327e5ba0df.tar.gz";
-    sha256 = "11l29m0dyvg1yvfj3blmaw4gnqyxi455jcx2rbh4m4rr1p8nzkhx";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/5573585739d49eb8e1fd040200aff753e0848ece.tar.gz";
+         sha256 = "0g2zzp6ycynmvmk0gr73ipnrwnjg4rs7crgs16j07qkxlj5qci6p";
+       }
