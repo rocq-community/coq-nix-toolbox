dd2f9fbbf6a4cced48b827233403758456839284
Pierre Roux <pierre.roux@onera.fr>
2025-07-31 12:00:30 +0200

    coqPackages.interval: 4.11.2 -> 4.11.3

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/151a2eced20fc3ced498e2314cce33487478bf75.tar.gz";
-    sha256 = "00q75ldz1yc1g03jlpxpka9vpr4qzmv53k9mbbkkdypn9266ybxa";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/38f1c3878774c78fb5fcb2cb7f8a17dbad4e8bda.tar.gz";
+         sha256 = "16vl30ckyq2fvvphy7w1iw0yyw5x68fgacc9k9nsdvk9f59hk3il";
+       }
