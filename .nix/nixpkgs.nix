998a6ae0e49c5b8fbbbb502f8d48fc60e031a3ae
Wolfgang Meier <womeier@posteo.de>
2025-07-31 20:45:26 +0200

    test wasmcert init

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/151a2eced20fc3ced498e2314cce33487478bf75.tar.gz";
-    sha256 = "00q75ldz1yc1g03jlpxpka9vpr4qzmv53k9mbbkkdypn9266ybxa";
-  }
+         url = "https://github.com/womeier/nixpkgs/archive/2c355cc4dcf28266e454ba1e30406e5dfb1286b3.tar.gz";
+         sha256 = "10iz1c716q95cqf8xcfczr873ccz844ghrn58z8pc92jm01y7qxv";
+       }
