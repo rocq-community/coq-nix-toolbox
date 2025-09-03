291f2bd8632eb4c8e1f16c3a793d334ef8d4564b
Pierre Roux <pierre.roux@onera.fr>
2025-09-03 09:45:01 +0200

    coqPackages.dpdgraph: 1.0+8.20 -> 1.0+9.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/02636a074486be07ece520241490e549af1453da.tar.gz";
-    sha256 = "01q755vb5p31962dxpc32l5zic1ffc8550pbx6d2wgjxf0c5f505";
+    url = "https://github.com/NixOS/nixpkgs/archive/74bfa3ef406c31850eaa2e9ffac6aab7bda2aef5.tar.gz";
+    sha256 = "0574s7zwdlxk4al4azhbad8kck8023kabqkn4mz9jg7piar60s4l";
   }
