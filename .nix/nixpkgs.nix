e2a3bf9d6868ad8591c19932aaaf7e68cef76b67
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-09-18 10:13:35 +0200

    autosubst-ocaml 1.1+9.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/d3244e63ff6db4c5e43e1af9ff3eeaa280345f5b.tar.gz";
-    sha256 = "1zz2zq5y3a12k6bc2kqvsar7ylg9zalvi5p16qcf4qc5pd4zvamc";
-  }
+         url = "https://github.com/ironm00n/nixpkgs/archive/a246280b51a96cad3a8449969d25dade07cf5de4.tar.gz";
+         sha256 = "04kikznxkdmqvnqbwr7308gifsa192rb96bjw0bnzsdbnd47zrxh";
+       }
