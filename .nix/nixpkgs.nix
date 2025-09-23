213a41753b34ad97e785f363ee7b679c6ade1af1
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-09-23 12:01:40 +0200

    coq-lsp 0.2.4

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/12bd230118a1901a4a5d393f9f56b6ad7e571d01.tar.gz";
-    sha256 = "07xmvvgrsah186nxppdvh5xcbpkcl8i1xh3dx6q3bchff7fsa4b8";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/df493310e8864a03c31f5b7a9fcf53ec32c6028d.tar.gz";
+         sha256 = "1y8mmj7hmhc1q8gr6c6j2hxhp8r64dwifbn9s8kmrkq6z6d1k7sx";
+       }
