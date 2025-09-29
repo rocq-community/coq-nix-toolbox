1e1dfbb387bf0db34a53821ef9c0f19851cd493d
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-09-29 11:40:25 +0200

    rocq 9.0.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9b00415732acb6a3077e624d0add14943517fd9b.tar.gz";
-    sha256 = "18n0izj2g9wk8j33gyf976gjqsldhwfhdsfnv8yhz5i1fg2wfsgq";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/19445eed7f9b284c37fbcaee69facbe2146ba2eb.tar.gz";
+         sha256 = "1xwy1cxizgg65wg3ph35zi1jpv23sn2ffbnsrmdf188dxfx4p7ga";
+       }
