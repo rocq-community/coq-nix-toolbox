a22e7aff5ea56647c12cc2590d3a78b3a9b15385
Vincent Laporte <vbgl@users.noreply.github.com>
2025-10-10 07:22:21 +0200

    coq-hammer 1.3.2 for Coq 9.0 (#400)
    Update after NixOS/nixpkgs#449881

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c4a27cc8147458be38c7ddc6b9265780f9fcd693.tar.gz";
-    sha256 = "0xdfvkyb14jpsf3w3s8qgcymbaddnb147r9d4jmjhd1b0sl3hwfs";
+    url = "https://github.com/NixOS/nixpkgs/archive/5bc997e77a793c93e232448e21733c75beb2fdac.tar.gz";
+    sha256 = "0k74xa05gjg4x5inccpil62x24m77grm7qyn21vcrahp5ispdwaw";
   }
