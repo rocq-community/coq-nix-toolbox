004ef954cdaf50aa4286c5becc5d9a84a38cd496
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-11-06 05:25:39 +0100

    equations 1.3.1+9.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/333b3224f6966d768886598b40c6d143e972e840.tar.gz";
-    sha256 = "0cpwgmpl94nr7p08rh0pwrxjsw9hf3ik504kfimla2p3ax26vs0l";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/24c4b5c451e2596fa3136254ab8dc36284dd49ea.tar.gz";
+         sha256 = "175chr00xnp6i39j52g9s7jblhblbjxpif1760a859pznmx4y66b";
+       }
