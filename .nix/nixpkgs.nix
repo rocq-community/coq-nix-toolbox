4f44ad4835736a836459b42864d4c6a098e16d74
Pierre Roux <pierre.roux@onera.fr>
2025-11-06 08:13:24 +0100

    coqPackages.metarocq: 9.0 -> 9.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/62dab73c6aa5a9da858ef26d29600541c85eed6d.tar.gz";
-    sha256 = "1mj9v5s2129h6ffg0awc9b39mh3l41nqm0mpnyvn8idmhxja8wbq";
+    url = "https://github.com/NixOS/nixpkgs/archive/d03ce9c0e98656cd9505aab8277cc299c1376a58.tar.gz";
+    sha256 = "0ygwclm9kyi2264z6syas61k8154yr6ny1604g2x52hs5aiz88dd";
   }
