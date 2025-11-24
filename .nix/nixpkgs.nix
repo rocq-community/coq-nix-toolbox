74151e10f0e4c94276de317b5632b347cceb5efb
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-11-24 05:19:30 +0100

    Test NixOS/nixpkgs#464460

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c6f2f0cd7b56d6c13582509d8f34e1e1decb9779.tar.gz";
-    sha256 = "0aq9y5ymphwdx7bfs081k51cv8fhkgac73da3hivn0ji3nypciqb";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/49ab2d4637e179dd5d6afc11cd7c6e313fe45303.tar.gz";
+         sha256 = "1ppqlhy8ghil4zz8lww36aaz0c3z4pg7hr92i44spgjhfad3j07w";
+       }
