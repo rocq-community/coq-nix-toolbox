2180901d108b99f1c55ecb721918d91ea2b00656
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-11-24 04:52:19 +0100

    Update after NixOS/nixpkgs#462523

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = "https://github.com/vbgl/nixpkgs/archive/74d49c55d13518c40f626767e2ed5d08860fb698.tar.gz";
-         sha256 = "103kfhna95q4nh2l99r54ngcv34nwpqmzqq6mm7dbhr8q2zg3rjl";
-       }
+    url = "https://github.com/NixOS/nixpkgs/archive/c6f2f0cd7b56d6c13582509d8f34e1e1decb9779.tar.gz";
+    sha256 = "0aq9y5ymphwdx7bfs081k51cv8fhkgac73da3hivn0ji3nypciqb";
+  }
