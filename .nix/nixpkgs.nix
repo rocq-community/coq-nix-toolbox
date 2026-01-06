84466e6a3da60854ac468a295f9ce9360d080c4b
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-01-06 13:50:42 +0100

    dpdgraph for Coq 9.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/44ac20273df09e0ea9a85d3776df059cf0c871d9.tar.gz";
-    sha256 = "0ysk2y41sq769lbqp8iq4yja0d9wxw53lm6fl805w5nqyxf29vr5";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/d825192c3ed0eed8328b08243e79ea1d92cf8922.tar.gz";
+         sha256 = "1wvkkz4ddj8rc11xvsd3lv6ggmd7q968hi7jpbmg8az72hpdsivp";
+       }
