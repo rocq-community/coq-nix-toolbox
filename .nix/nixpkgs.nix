8961ad6ac93eacbc95bafcc2d92e435682f09928
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-01-08 22:57:51 +0100

    Test nixpkgs#478205

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/1fb7dba0e0e3f10ffde0394481540ad71444f8d2.tar.gz";
-    sha256 = "03yl0b9xglq1bf81x1cm87fqf022h6v2awqiifaw7hrabllpsfak";
+    url = "https://github.com/NixOS/nixpkgs/archive/72d091368fa95171debdb1b7b315f12a23528cd4.tar.gz";
+    sha256 = "1xqi6sb3376rfsmckvxjk2g6wcmcy7h318qdnnk9cmz6wcgms0f3";
   }
