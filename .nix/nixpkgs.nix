ee017112cf5c1434db07a5ad71cee78e21f2fa1e
Théo Zimmermann <theo.zimmermann@inria.fr>
2026-02-04 08:02:45 +0100

    Test simple-io update. (#416)
    Update after NixOS/nixpkgs#486548

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/c8f82e8bf3014446303739c7e6acacafc8294161.tar.gz";
-    sha256 = "0mfjs0c2jy4kwn2fjvj9lgxq3z6z3ykrhz8jnz8cfx5f9x6xjbfd";
+    url = "https://github.com/NixOS/nixpkgs/archive/fad7370009d911c9dbfbfd24ab8f609f1fd6e896.tar.gz";
+    sha256 = "1mv41m6i0i278w0ps6m92y8z8dbdnqrixfmfi83l52n477wf0kgz";
   }
