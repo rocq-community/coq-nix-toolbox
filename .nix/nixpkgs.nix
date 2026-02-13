61af5f23aa27ea1f62be15190be61b2fa5b5b8a1
Vincent Laporte <vbgl@users.noreply.github.com>
2026-02-13 14:44:37 +0100

    CompCert 3.17 (#421)
    Update after NixOS/nixpkgs#490095

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/5bc0e6a8a2fd3878d77a9083f3f0a76d0614ef39.tar.gz";
-    sha256 = "0kslsvagv7s1fjw8ly9plr8h5ksj9p3mpm30v7a9gllya5jsq09v";
+    url = "https://github.com/NixOS/nixpkgs/archive/44daa4fa27a558fce1f34cd62f5895a99913f380.tar.gz";
+    sha256 = "0sgmn10r58jc8csq2x4qb3njwk37a4aqhyzv5g2ymvr6mlzqrc82";
   }
