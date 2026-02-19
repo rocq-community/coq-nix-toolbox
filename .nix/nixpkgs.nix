1e73f8b9d35024298b64311fabfefd6cc8436401
Pierre Roux <pierre.roux@onera.fr>
2026-02-19 13:00:26 +0100

    rocqPackages/.mathcomp-bigenough: oinit at 1.0.4

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/44daa4fa27a558fce1f34cd62f5895a99913f380.tar.gz";
-    sha256 = "0sgmn10r58jc8csq2x4qb3njwk37a4aqhyzv5g2ymvr6mlzqrc82";
+    url = "https://github.com/NixOS/nixpkgs/archive/09ac1a4dd614f93d8588def74847f810cc9b329c.tar.gz";
+    sha256 = "1k8zxrfkqgab5kscn2nkn48g7qgjb2qjqrgv48gpq9dx5848pbnd";
   }
