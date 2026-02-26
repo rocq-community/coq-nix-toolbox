4fa927a942efb0af76857136ae66bba93d5aa031
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-02-26 11:38:24 +0100

    TypedExtraction 0.2.0
    Test nixpkgs#494153

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6077527412d8618f4a30a121223687698fe6be25.tar.gz";
-    sha256 = "0qscb1qr69p6l848j0lc9nxi44yzqlyzg6xdm3zzpzq24d149icy";
+    url = "https://github.com/NixOS/nixpkgs/archive/459f4774860ade37fae0b035636625c0c92eeb36.tar.gz";
+    sha256 = "1qpw74zvsilnm6ixgjp9bimxqsmyhps9wzkipwsxcaiig51h6lsz";
   }
