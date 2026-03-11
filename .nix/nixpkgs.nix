e78dfa7bbb7e8073ff287e658b0af3f0e25b23cd
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-11 23:47:14 +0100

    SSProve v0.3.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/78fd69c6d275e8a0e699cb4453903d3ad0ace985.tar.gz";
-    sha256 = "0qypsdrlabww8nn77dvwjnklkwlai961dxsisgbr3sar087kj8zz";
+    url = "https://github.com/NixOS/nixpkgs/archive/b9637319b2e3b8061ba416b46cde41bd9f8b4442.tar.gz";
+    sha256 = "1nk2cljmlbl2ps3ln864bpdj4kxczss7fffap6pmik40k0wsnvvy";
   }
