9b91cd0afedc7721be5f66a42a8d8b10374714dc
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-11 17:19:02 +0100

    ceres-bs 1.0.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/78fd69c6d275e8a0e699cb4453903d3ad0ace985.tar.gz";
-    sha256 = "0qypsdrlabww8nn77dvwjnklkwlai961dxsisgbr3sar087kj8zz";
-  }
+         url = "https://github.com/4ever2/nixpkgs/archive/37b3d4d7e1df4b4f46a91a1c6b01b7b3ec801be8.tar.gz";
+         sha256 = "0pna5lffl6c06z88nlrp8qywfc1c2yawpm2brb8jg6kampb0d1yb";
+       }
