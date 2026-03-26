3e11db1b014f5f6bedf9322eed22ef325c868f9b
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-26 10:59:17 +0100

    verified-extraction v1.0.0 (#446)
    Update after NixOS/nixpkgs#503111

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/b3f8d82c4c685fb6f3080745dab8f07606ae50d3.tar.gz";
-    sha256 = "0bf4yvzc9szslmzfjjlz03cf75571f0ddb36l0ndgzlipx63ai74";
+    url = "https://github.com/NixOS/nixpkgs/archive/782f2fde1c9b91998ac56c859d2c398fe53a4764.tar.gz";
+    sha256 = "0kl8mj6faykh930lmh0cd001648xj18krnvsfmysc13qn99j5v9v";
   }
