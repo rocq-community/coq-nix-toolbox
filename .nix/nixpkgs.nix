77466a366b52ee9a0477c6f97eebcf549c0e8cd6
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-03-30 07:52:51 +0200

    ExtLib 0.13.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/25ac7a9118cb69f709242a2a2f5db9dbc55c479f.tar.gz";
-    sha256 = "00li6mlhrqafs2i2h6yddmwdsjn60bpi8pvvxnpxay5w8lwmmadq";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/afc3c1ea88efd21ab6c572fb51b45d4aea4dc01c.tar.gz";
+         sha256 = "1wki8hldmdrh5nr8n3a09hkjakm55dsdhsb8bi4xgcyka51biibz";
+       }
