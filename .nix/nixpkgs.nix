48e99947f03ceadef1e5ae1945e733a839a11c2a
Vincent Laporte <vbgl@users.noreply.github.com>
2026-04-02 20:21:04 +0200

    More libraries for Coq 9.1 & 9.2 (#449)
    Update after NixOS/nixpkgs#505435

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f8dc457643e46830c850cab9e451ab3bbfbbee1b.tar.gz";
-    sha256 = "0fvkv0qz9vlq1if55fr6xf4ysn0r24w73a2w9n19sipdapn7np72";
+    url = "https://github.com/NixOS/nixpkgs/archive/7a7e0324a83cad41c2f6696b0f77990135f4d95c.tar.gz";
+    sha256 = "1w4i1frzqmh9i5ckaid8m3faksqzrvzilpl53qdgz25mag1fpi1r";
   }
