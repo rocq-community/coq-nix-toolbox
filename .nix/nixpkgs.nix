00c376ca389a2f742e1bf6c472e035871cb023e6
Vincent Laporte <vbgl@users.noreply.github.com>
2026-04-15 13:51:12 +0200

    Jasmin 2026.03.1 (#451)
    Update after NixOS/nixpkgs#509942

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2cce2415f39a7845cf97c823e7135f44f4eff809.tar.gz";
-    sha256 = "0i4k6mmf4v8h550f7nr0pjvzrmaan331bkyqg9h21df4axc1r0di";
+    url = "https://github.com/NixOS/nixpkgs/archive/1ba325b65d53b87872ddd16b9c225f1b290100f8.tar.gz";
+    sha256 = "1h7kim0rnd6iyjq2y509kbakiqjhi75j0fsb4sm5f4741lqgh13b";
   }
