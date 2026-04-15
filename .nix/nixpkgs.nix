71e4a955f290302e843c5caaeb37197e0f708fc4
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2026-04-15 14:03:52 -0400

    Rollback coqPackages_8_20.coq-elpi to avoid bug on aarch64.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/1ba325b65d53b87872ddd16b9c225f1b290100f8.tar.gz";
-    sha256 = "1h7kim0rnd6iyjq2y509kbakiqjhi75j0fsb4sm5f4741lqgh13b";
-  }
+         url = "https://github.com/arthuraa/nixpkgs/archive/4cf17f460f84a2fb676efa93661cc6be37715a31.tar.gz";
+         sha256 = "19z0p7f7avmxsmigk96rskhrpwkdnfc843wkyhzwzxfw8a559dl7";
+       }
