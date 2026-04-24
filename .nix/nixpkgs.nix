b777049f817b9846b0de82a7247c833e0c59feb6
Vincent Laporte <vbgl@users.noreply.github.com>
2026-04-24 10:45:17 +0200

    Ordinal 0.5.6 (#456)
    Update after NixOS/nixpkgs#496086

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6f37a0d0b224a680a89e4777940a80b86f7cdadd.tar.gz";
-    sha256 = "0l8mg6kvs5j139mad04ibr1h7pinw5jjafs1xsvn1zj25sjyc9h5";
+    url = "https://github.com/NixOS/nixpkgs/archive/bac2342c060290753cb5d977f4e929cfc8e253ef.tar.gz";
+    sha256 = "07r2lmbrjx6xqw4cs9jqaw1nyz954x2cn7cw6idph4mx45xzbp2h";
   }
