c643f85a577e9341939a172877932962593379e4
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-04-24 06:36:49 +0200

    Ordinal 0.5.6

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6f37a0d0b224a680a89e4777940a80b86f7cdadd.tar.gz";
-    sha256 = "0l8mg6kvs5j139mad04ibr1h7pinw5jjafs1xsvn1zj25sjyc9h5";
-  }
+         url = "https://github.com/damhiya/nixpkgs/archive/719809a40484c8d091fd62e740c38676d0c606e5.tar.gz";
+         sha256 = "192k8mphlcgklywsa6sa011j7x6m4lis7pb5m1df4ybzkx5i7k2v";
+       }
