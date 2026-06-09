8613db3eb9ba7d69a547d00816286ca527e86b85
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-06-09 07:23:12 +0200

    Dune 3.22.2

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/815e8f98dfe4a7958a29577110a32dc8a42ec8bb.tar.gz";
-    sha256 = "0ccnc9z8fdw623iagb0sqwllwa1dnwij59j7yzm72qfgdx0ay686";
+    url = "https://github.com/NixOS/nixpkgs/archive/e2149785fe58397fc7c40cfce90557357756b254.tar.gz";
+    sha256 = "11kwyv9r6cfhlfb5bw7r0rbf8gzdaz82z448hjs8idi77f1n2f49";
   }
