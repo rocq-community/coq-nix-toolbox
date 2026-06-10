f191e26df03e91dbbe2672c338e45fe62b7f0b81
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-06-10 22:25:22 +0200

    Dune 3.23.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/e2149785fe58397fc7c40cfce90557357756b254.tar.gz";
-    sha256 = "11kwyv9r6cfhlfb5bw7r0rbf8gzdaz82z448hjs8idi77f1n2f49";
+    url = "https://github.com/NixOS/nixpkgs/archive/0ee9c4c5da04cf1bfc1568f8c30528e1fa19dcce.tar.gz";
+    sha256 = "0vvx0bccswxs9nc1s2b77ggymfbgs6iwlya10vkkq3jp5mnfnnjy";
   }
