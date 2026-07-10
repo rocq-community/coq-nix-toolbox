45ec67d983f6ce4a10f0649cc1b2eeac0459b498
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-07-10 06:25:54 +0200

    Test NixOS/nixpkgs#537856

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/1e94a0307f544377e2f2332daa7fca2833a1adc3.tar.gz";
-    sha256 = "0yhc94slrq5xlbnilwq0wd6j9ca4vcd33bvwf27ddbmdai76fl2y";
-  }
+         url = "https://github.com/Zimmi48/nixpkgs/archive/962d8fb3ed9d715fcefc7b831056ca18b08e1896.tar.gz";
+         sha256 = "1d8bgladkni2grqll6fzvgj58mz79fi7dpc7ydpamvyk8zi70752";
+       }
