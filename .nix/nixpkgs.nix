bf88b1b76c6ecae153c2dcebbe69f39b0e50067a
Pierre Roux <pierre.roux@onera.fr>
2026-07-24 08:32:40 +0200

    rocqPackages.rocq-elpi: 3.4.0 -> 3.5.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/335f0738cb2fa9708f3f428e39d2eae975d1338d.tar.gz";
-    sha256 = "15ac9jjvzdqkxwjzldzpj46caibvh2gvc3f83wlgplb8d8bw8wsc";
+    url = "https://github.com/NixOS/nixpkgs/archive/a76040f2dfc63651fbe557ea87cdd68aef8dc1c5.tar.gz";
+    sha256 = "105pcfvrixjd88r3aamvmclbv2cyd2i74ygp7xyqwykx0wpqm4jn";
   }
