9efa43c86708c44325c036b65c2088354b38ae12
Vincent Laporte <vbgl@users.noreply.github.com>
2026-07-24 10:32:26 +0200

    mathcomp-word 3.5 (#491)
    Update nixpkgs after NixOS/nixpkgs#544910

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f15235f5ae4ef6201212136e114207f0dd9775ff.tar.gz";
-    sha256 = "1agfw08h550cj1bvyz087bwbqpmi3xwi9725cmmlqy63sxaf694j";
+    url = "https://github.com/NixOS/nixpkgs/archive/335f0738cb2fa9708f3f428e39d2eae975d1338d.tar.gz";
+    sha256 = "15ac9jjvzdqkxwjzldzpj46caibvh2gvc3f83wlgplb8d8bw8wsc";
   }
