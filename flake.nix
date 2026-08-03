735b6a093efaa3f5b290219699c397a63a7cb1dd
Mysaa Java <mysaa@hadoly.fr>
2026-08-03 19:16:06 +0200

    Nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=master";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: let
