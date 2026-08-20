449b528e1514d50dce53b44ea0a955e9b066b8ca
Hugo Martel <hugo.martel@ens-lyon.fr>
2026-08-20 15:01:14 +0200

    rocqPackages.rocqide: init at 9.1.1
    Also adds rocqPackages.rocqide-server

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/391b592eb44808b3bd0cb80bb71b63a5a118b8bb.tar.gz";
-    sha256 = "0sf8gv1r89ahw358swal14pjk5b7xvx0p34bnqdv0h0ji53wgyss";
-  }
+         url = "https://github.com/HugoMartel/nixpkgs/archive/a01ef068105b4011f9938ee14e642f480e3abf6c.tar.gz";
+         sha256 = "0yfqfk77qdh50zp8ly6sv7xbg13sni7s5ip4xwabr2lpjhb6s2nh";
+       }
