1d05873ec7d9be86c51144e9e9c49f62994649c9
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-08-24 16:42:41 +0200

    ITree for Rocq 9.2

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = "https://github.com/vbgl/nixpkgs/archive/9ad1af907307f832a516caf0574545d0d90e7172.tar.gz";
-         sha256 = "0lz5c0zsgsx6zjyfm4rh3n4nf9ir8gqg561b0qj78mnybix4ha22";
+         url = "https://github.com/vbgl/nixpkgs/archive/829bbb6005a033fe295dd679aaffba67bdfd575a.tar.gz";
+         sha256 = "1cvzsycar83zgg26084ljynlv1hpy5x0m3ck0nz5875q5ghf7s0x";
        }
