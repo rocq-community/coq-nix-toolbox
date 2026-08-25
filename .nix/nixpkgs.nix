2965625ca913da45ff301668c51cdfb10e76f63d
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-08-25 16:18:49 +0200

    Math-classes 9.2.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f7078916123035fb0b73afda1ae514c90acb7f36.tar.gz";
-    sha256 = "0frnfadnj8k0fl5grxf0rw222xi558lrnhb6jz77rhzyf5705ld1";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/b21702d5510f5e619192dd2a63609b4979d4f542.tar.gz";
+         sha256 = "1zf4c9cljf7r41p7yn6v2360fmk34nm1z83xrlrl20arh910n3h8";
+       }
