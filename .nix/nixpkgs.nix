272c2a2013b7ca85e514889b693b8f6419f87d58
Vincent Laporte <vbgl@users.noreply.github.com>
2026-08-25 20:00:14 +0200

    Math-classes 9.2.0 (#503)
    Update after NixOS/nixpkgs#556417

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f7078916123035fb0b73afda1ae514c90acb7f36.tar.gz";
-    sha256 = "0frnfadnj8k0fl5grxf0rw222xi558lrnhb6jz77rhzyf5705ld1";
+    url = "https://github.com/NixOS/nixpkgs/archive/730ed47e070853acb8f613b12af374061c3fbdf0.tar.gz";
+    sha256 = "0xh232vbhd59630hs208l6bydnl7f46sfgvwqw1jjwaq2nqgk7s5";
   }
