80f5d1078dd4a1df0b57e4c3e654301db704357f
Vincent Laporte <vbgl@users.noreply.github.com>
2026-08-26 22:40:50 +0200

    Update relation-algebra (#504)
    * relation-algebra 1.8.1
    
    * relation-algebra 1.9.0
    
    * Update nixpkgs after NixOS/nixpkgs#556748

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/730ed47e070853acb8f613b12af374061c3fbdf0.tar.gz";
-    sha256 = "0xh232vbhd59630hs208l6bydnl7f46sfgvwqw1jjwaq2nqgk7s5";
+    url = "https://github.com/NixOS/nixpkgs/archive/75133f97305a257c7e14882fd2761e885ef844a4.tar.gz";
+    sha256 = "0d4b64sz1j32hjv88vf5n2p3zfkcdqw3wzs5412ngqdqxji3whs7";
   }
