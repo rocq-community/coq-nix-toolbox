b6ebd4942644d1a5eaf91d39f6566e5fa862ae1b
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-08-26 15:20:46 +0200

    relation-algebra 1.8.1

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/730ed47e070853acb8f613b12af374061c3fbdf0.tar.gz";
-    sha256 = "0xh232vbhd59630hs208l6bydnl7f46sfgvwqw1jjwaq2nqgk7s5";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/78e9ddde2f3bbabe94f566d7439c05e21356e169.tar.gz";
+         sha256 = "056pbm10dc095wva6f9nsk1218f66x9sbviq7n2m8bzka60xg73b";
+       }
