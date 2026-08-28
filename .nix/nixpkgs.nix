c627e598130db00455789dc96538225bd4257169
Pierre Roux <pierre.roux@onera.fr>
2026-08-28 08:33:55 +0200

    rocqPackages: update coquelicot and mczify
    Following merge of https://github.com/NixOS/nixpkgs/pull/557263

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/75133f97305a257c7e14882fd2761e885ef844a4.tar.gz";
-    sha256 = "0d4b64sz1j32hjv88vf5n2p3zfkcdqw3wzs5412ngqdqxji3whs7";
+    url = "https://github.com/NixOS/nixpkgs/archive/9af41c44c460a362f9771feb27d7def7cc0be758.tar.gz";
+    sha256 = "0pfz6xk0mywfx5f371hsma60cw065mpr37cpx09bfhjyzlkx7663";
   }
