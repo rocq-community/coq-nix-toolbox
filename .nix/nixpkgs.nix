18b8ccfca374c2d2c31ba0a62b09d0280e735a14
Pierre Roux <pierre.roux@onera.fr>
2026-09-03 09:00:49 +0200

    rocqPackages.mathcomp-analysis: 1.16.0 -> 1.181.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9af41c44c460a362f9771feb27d7def7cc0be758.tar.gz";
-    sha256 = "0pfz6xk0mywfx5f371hsma60cw065mpr37cpx09bfhjyzlkx7663";
+    url = "https://github.com/NixOS/nixpkgs/archive/bd7c030696f60d36593a19b21f8428d102bc49bd.tar.gz";
+    sha256 = "0m5v6bl1blqyrrwvgg0an58nnxz9ql4ijj5vmxx4dy8pq5kddzjm";
   }
